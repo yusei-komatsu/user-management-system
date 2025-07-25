42d95f97fb35a71e392bf32f05a3d861fd20f8a0
yusei-komatsu <yusei.komatsu@fox-hound.jp>
2025-07-25 16:33:53 +0900

    タスク1-3完了

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -15,6 +15,10 @@ const Navbar: React.FC = () => {
         <Button color="inherit" component={Link} href="/users">
           ユーザー一覧
         </Button>
+        {/* 新規登録登録ページへのリンク（タスク1-3-1.b） */}
+        <Button color="inherit" component={Link} href="/register">
+          新規登録
+        </Button>
       </Toolbar>
     </AppBar>
   );
